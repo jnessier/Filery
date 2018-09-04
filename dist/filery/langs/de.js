986162c1798df0ebe6d59de87ee0a7d8c72b3c73
rjgamer <jonathan.nessier@neoflow.ch>
2018-09-04 23:32:31 +0200

    Plugin refactoring

---
+++ dist/filery/langs/de.js
@@ -0,0 +1,29 @@
+tinymce.addI18n('de', {
+    'File manager': 'Dateiverwaltung',
+
+    'Delete': 'Löschen',
+    'Select': 'Auswählen',
+    'Upload': 'Hochladen',
+    'Insert link': 'Link einfügen',
+    'Insert image': 'Bild einfügen',
+    'Cancel': 'Abbrechen',
+
+    'Are you sure you want to delete "{0}"?': 'Bist du sicher, dass du "{0}" löschen willst?',
+
+    'Upload failed.': 'Hochladen fehlgeschlagen.',
+    'Delete failed.': 'Löschen fehlgeschlagen.',
+    'Load failed.': 'Laden fehlgeschlagen.',
+
+    '"{0}" successfully uploaded.': '"{0}" erfolgreich hochgeladen.',
+    '"{0}" successfully deleted.': '"{0}" erfolgreich gelöscht.',
+    '"{0}" successfully deleted. Do you want to remove the content with reference to the deleted file?': '"{0}" erfolgreich gelöscht. Willst du den Inhalt mit Bezug zur gelöschten Datei entfernen?',
+    '"{0}" successfully inserted.': '"{0}" erfolgreich eingefügt.',
+
+    '"{0}" as image successfully inserted.': '"{0}" als Bild erfolgreich eingefügt.',
+    '"{0}" as link successfully inserted.': '"{0}" als Link erfolgreich eingefügt.',
+
+    'File already exists.': 'Die Datei existiert bereits.',
+    'File is too large.': 'Die Datei ist zu gross.',
+    'File extension is not allowed.': 'Die Dateierweiterung ist nicht erlaubt.'
+
+});
