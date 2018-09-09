e1229618bed6db1c630ccaba5666455fb0751a58
rjgamer <jonathan.nessier@neoflow.ch>
2018-09-09 17:34:07 +0200

    Fixed token handling

--- api/classes/Filery/AbstractAPI.php
+++ api/classes/Filery/AbstractAPI.php
@@ -19,8 +19,8 @@ abstract class AbstractAPI
         ],
         'showHiddenFiles' => false,
         'accessControl' => [
-            'allowedOrigin' => '*',
-            'allowedMethods' => 'GET, POST, DELETE, PUT, OPTIONS',
+            'acceptedOrigins' => [],
+            'acceptedMethods' => 'GET, POST, DELETE, PUT, OPTIONS',
         ],
         'fileTypes' => [
             'code' => ['java', 'php', 'html', 'js', 'css', 'htm', 'cpp', 'ts', 'xml', 'json', 'bat'],
@@ -88,13 +88,16 @@ abstract class AbstractAPI
             $this->cors();
 
             try {
+                if (!isset($_SERVER['HTTP_X_FILERY_TOKEN'])) {
+                    throw new Exception('Token not sent.');
+                }
                 $token = new Token();
                 $customConfig = $token
                     ->set($_SERVER['HTTP_X_FILERY_TOKEN'])
                     ->fetchFromSession();
                 $this->config = array_replace_recursive($this->config, $customConfig);
-            } catch (Exception $ex) {
-                throw new HttpException(401, $ex->getMessage());
+            } catch (Throwable $ex) {
+                throw new HttpException(401);
             }
 
             if (!is_readable($this->config['base']['path'])) {
@@ -104,9 +107,7 @@ abstract class AbstractAPI
             $output = $this->call();
         } catch (HttpException $ex) {
             http_response_code($ex->getCode());
-            $output = [
-                'error' => $ex->getMessage(),
-            ];
+            $output = ['error' => $ex->getMessage()];
         } catch (Throwable $ex) {
             http_response_code(500);
             $output = [
@@ -126,13 +127,19 @@ abstract class AbstractAPI
      */
     protected function cors()
     {
-        header('Access-Control-Allow-Origin: '.$this->config['accessControl']['allowedOrigin']);
-        header('Access-Control-Allow-Credentials: true');
         header('Access-Control-Max-Age: 86400');
+
+        if (isset($_SERVER['HTTP_ORIGIN']) && count($this->config['accessControl']['acceptedOrigins'])) {
+            if (in_array($_SERVER['HTTP_ORIGIN'], $this->config['accessControl']['acceptedOrigins'])) {
+                header('Access-Control-Allow-Origin: '.$_SERVER['HTTP_ORIGIN']);
+            } else {
+                throw new HttpException(403);
+            }
+        }
 
         if ('OPTIONS' == $_SERVER['REQUEST_METHOD']) {
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD'])) {
-                header('Access-Control-Allow-Methods: '.$this->config['accessControl']['allowedMethods']);
+                header('Access-Control-Allow-Methods: '.$this->config['accessControl']['acceptedMethods']);
             }
 
             if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS'])) {
