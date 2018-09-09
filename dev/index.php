0ea2aa1f00655070a822add16fa26f043f4cd311
rjgamer <jonathan.nessier@neoflow.ch>
2018-09-09 17:22:02 +0200

    Reorganized dev server

--- dev/index.php
+++ dev/index.php
@@ -2,6 +2,19 @@
 <html>
 <head>
     <script src="//cdn.tinymce.com/4/tinymce.min.js"></script>
+    <script src="http://localhost:8080/scripts/main.js"></script>
+    <?php
+    session_start();
+
+    require_once '../api/classes/Filery/Token.php';
+    $token = new Filery\Token();
+    $token->create([
+        'base' => [
+            'path' => realpath(__DIR__.'/storage/custom'),
+            'url' => 'http://filery.local/dev/storage/custom',
+        ],
+    ]);
+    ?>
     <script>
         tinymce.init({
             selector: 'textarea',
@@ -12,21 +25,26 @@
             ],
 
             contextmenu: 'image link',
-            language_url: 'http://filery.local/static/langs/de.js',
             height: '500px',
             toolbar: 'filery | insert | image',
             file_picker_types: 'file image media',
 
             image_advtab: true,
             imagetools_toolbar: 'editimage imageoptions',
+
             imagetools_cors_hosts: ['filery.local'],
 
+            // Don't forget to set the Filery callback handler of images_upload_handler
             automatic_uploads: true,
+
+            // Don't forget to enable upload.overwrite in your API config to get this work with Filery
             images_reuse_filename: true,
+
+            // Alternative to images_upload_handler callback, but not supported by Filery
             //images_upload_url: 'http://filery.local/api/index.php?images_upload_url=1',
 
             filery_dialog_height: '400px',
-            filery_api_token: false,
+            filery_api_token: '<?= $token->toString(); ?>',
             filery_api_url: 'http://filery.local/api/index.php',
 
             file_picker_callback: function (callback, value, meta) {
@@ -37,7 +55,6 @@
             }
         });
     </script>
-    <script src="../src/langs/de.js"></script>
 </head>
 <body>
 <textarea>Testing filery.</textarea>
